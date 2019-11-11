a7c9caa19cf6834be5f573091db666cd386e9174
Salina <amargunjal28@gmail.com>
2019-11-11 19:29:04 +0530

    11/11 eve

--- blabz/jdbc/DatabaseConnectivity.java
+++ blabz/jdbc/DatabaseConnectivity.java
@@ -4,15 +4,17 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class DatabaseConnectivity {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 		try {
 			Scanner scanner = new Scanner(System.in);
 			int choice;
 			do {
 			System.out.println("1.Read data");
-			System.out.println("2.Insert Values");
-			System.out.println("3.Delete Values");
-			System.out.println("4.Update Values");
+			System.out.println("2.Insert Value");
+			System.out.println("3.Delete Value");
+			System.out.println("4.Update Value");
+			System.out.println("5.Create Table");
 			choice = scanner.nextInt();
 			String url = "jdbc:mysql://localhost:3306/demo";
 			String uname = "root";
@@ -52,10 +54,19 @@ public class DatabaseConnectivity {
 				
 				
 			case 4:
-				String query3="UPDATE studentinfo  SET sname = 'mayur' where sid=4";
+				String query3="UPDATE studentinfo SET sname = 'mayur' where sid=4";
 				int count2=stmt.executeUpdate(query3);
 				System.out.println(count2 + "rows affected");
+				break;
+				
+				
+			case 5:
+				String query4="CREATE TABLE demo.employee2(eid VARCHAR(10),ename VARCHAR(20))";              
+				stmt.executeUpdate(query4);
+				System.out.println("Tabel Created");
+				break;
 			}
+			System.out.println();
 			stmt.close();
 			con.close();
 			}while(choice!=6);
