67d3cb63b26300270dc08eb279f3ce50b4f3113a
Salina <amargunjal28@gmail.com>
2019-11-11 19:04:56 +0530

    11/11 eve

---
+++ blabz/jdbc/DatabaseConnectivity.java
@@ -0,0 +1,66 @@
+package com.blabz.jdbc;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class DatabaseConnectivity {
+	public static void main(String[] args) throws Exception {
+		try {
+			Scanner scanner = new Scanner(System.in);
+			int choice;
+			do {
+			System.out.println("1.Read data");
+			System.out.println("2.Insert Values");
+			System.out.println("3.Delete Values");
+			System.out.println("4.Update Values");
+			choice = scanner.nextInt();
+			String url = "jdbc:mysql://localhost:3306/demo";
+			String uname = "root";
+			String pass = "password";
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, uname, pass);
+			// Statement st = con.createStatement();
+			PreparedStatement stmt = (PreparedStatement) con
+					.prepareStatement("insert into studentinfo values(1,'abc')");
+			switch (choice) 
+			{
+			case 1:
+
+				String query = "select * from studentinfo ";
+				ResultSet rs = stmt.executeQuery(query);
+				while (rs.next()) {
+					String udata = rs.getInt(1) + " " + rs.getString(2);
+					System.out.println(udata);
+					
+				}
+				break;
+				
+				
+			case 2:
+				
+				String query1="insert into studentinfo values(4,'satish')";
+				int count = stmt.executeUpdate(query1);
+				System.out.println(count+" rows affected");
+				break;
+				
+			case 3:
+				String query2="delete from studentinfo where(sname = 'satish')";
+
+				int count1=stmt.executeUpdate(query2);
+				System.out.println(count1+ "rows affected");
+				break;
+				
+				
+			case 4:
+				String query3="UPDATE studentinfo  SET sname = 'mayur' where sid=4";
+				int count2=stmt.executeUpdate(query3);
+				System.out.println(count2 + "rows affected");
+			}
+			stmt.close();
+			con.close();
+			}while(choice!=6);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
---
+++ blabz/visitor/ItemElement.java
@@ -0,0 +1,7 @@
+package com.blabz.visitor;
+
+public interface ItemElement
+{
+	public int accept(ShoppingCartVisitor visitor);
+	
+}
