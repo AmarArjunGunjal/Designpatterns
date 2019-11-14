07b77e5d8b389883b6317bead04ad3c0424a2306
Salina <amargunjal28@gmail.com>
2019-11-14 10:52:43 +0530

    14/11 mor

--- blabz/jdbc/DatabaseConnectivity.java
+++ blabz/jdbc/DatabaseConnectivity.java
@@ -47,7 +47,7 @@ public class DatabaseConnectivity {
 
 					String query1 = "insert into studentinfo values(22,'dipak')";
 					int count = stmt.executeUpdate(query1);
-					
+
 					System.out.println(count + " rows affected");
 					con.commit();
 					break;
@@ -79,48 +79,45 @@ public class DatabaseConnectivity {
 					String queryr = "UPDATE studentinfo SET sname = 'qweee' where sid=1";
 					st.addBatch(queryr);
 
-					int count3[] = st.executeBatch();
+					st.executeBatch();
 					con.commit();
 					System.out.println("Applied");
 					break;
 
 				case 7:
 					int ch;
-					do
-					{	
-					System.out.println("Enter sid");
-					int sid = scanner.nextInt();
-					System.out.println("Enter sname");
-					String sname = scanner.next();
-					stmt.setInt(1, sid);
-					stmt.setString(2, sname);
-					stmt.addBatch();
-					int count4[]=stmt.executeBatch();
-					con.commit();					
-					System.out.println("record saved");
-					System.out.println("Want to add more record press 1");
-					 ch=scanner.nextInt();
-					}while(ch!=0);
-					
+					do {
+						System.out.println("Enter sid");
+						int sid = scanner.nextInt();
+						System.out.println("Enter sname");
+						String sname = scanner.next();
+						stmt.setInt(1, sid);
+						stmt.setString(2, sname);
+						stmt.addBatch();
+
+						stmt.executeBatch();
+						con.commit();
+						System.out.println("record saved");
+						System.out.println("Want to add more record press 1");
+						ch = scanner.nextInt();
+					} while (ch != 0);
+
 					break;
 
 				case 8:
 					con.commit();
 					System.out.println("commiting here");
 					break;
-					
+
 				case 9:
 					String query6 = "delete from studentinfo where(sname = 'asddaQW')";
-					int count9=stmt.executeUpdate(query6);
-					System.out.println(count9+"rows affected");
-					if(con!=null)
-					{
+					int count9 = stmt.executeUpdate(query6);
+					System.out.println(count9 + "rows affected");
+					if (con != null) {
 						con.rollback();
 						System.out.println("rollbacked");
 					}
-					
-						
-					
+
 				}
 				System.out.println();
 				stmt.close();
